266d3fd76f076648909a5d6e70ba0f9d18a41233
theqwe <micke_gnospelius@hotmail.com>
2014-02-05 14:56:16 +0100

    hej

---
+++ src/se/kth/csc/iprog/dinnerplanner/swing/view/testdinner.java
@@ -0,0 +1,42 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+
+public class testdinner {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					testdinner window = new testdinner();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public testdinner() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
